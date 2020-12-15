d7053838ec26dfafef5bb71059a514aba107a8eb
Rajesh Dhiman <rajeshdhimanskt@gmail.com>
2020-12-15 20:09:37 +0530

    created person properties and methods

--- index.js
+++ index.js
@@ -37,14 +37,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  Person.prototype.eat = function (someFood) {
+      if(this.stomach.length >= 10){
+        return;
+      }
+      this.stomach.push(someFood);
     
   }
- 
- 
 
-  
-  
+  Person.prototype.poop = function () {
+    this.stomach = []
+  }
+
+  Person.prototype.toString = function () {
+    return `${this.name}, ${this.age}`;
+  }
   
   
   /*
